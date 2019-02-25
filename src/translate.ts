fd488c76c678660e2531b3268a39954c90ad010e
gaoletian <gaoletian@finupgroup.com>
2019-02-25 16:25:28 +0800

    fix: translate engine with google or  baidu or youdao

--- src/translate.ts
+++ src/translate.ts
@@ -1,13 +1,15 @@
 import * as _ from 'lodash';
-const { google } = require('translation.js');
+const { youdao, baidu, google } = require('translation.js');
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as os from 'os';
+import * as debugLog from './debugLog';
 
 export class Translate {
   private localDictDir = os.homedir() + '/.pont';
   private dict: { [key: string]: string } = {};
   private dictFullPath = '';
+  private engines = [google, youdao, baidu];
 
   constructor(dictName = 'dict.json') {
     fs.mkdirpSync(this.localDictDir);
@@ -22,23 +24,41 @@ export class Translate {
   }
 
   appendToDict(pairKey: { cn: string; en: string }) {
-    fs.appendFileSync(this.dictFullPath, `"${pairKey.cn}": "${pairKey.en}",\n`);
+    if (!this.dict[pairKey.cn]) {
+      this.dict[pairKey.cn] = pairKey.en;
+      fs.appendFileSync(this.dictFullPath, `"${pairKey.cn}": "${pairKey.en}",\n`);
+    }
   }
 
-  async translateAsync(text: string) {
-    if (this.dict[text]) return this.dict[text];
-
-    let res = await google.translate(text);
-
-    let wordArray = _.startCase(res.result[0]).split(' ');
+  startCaseClassName(result) {
+    let wordArray = _.startCase(result).split(' ');
     if (wordArray.length > 6) {
       wordArray = [].concat(wordArray.slice(0, 5), wordArray.slice(-1));
     }
+    return wordArray.join('');
+  }
 
-    let enKey = wordArray.join('');
+  async translateAsync(text: string, engineIndex = 0) {
+    if (this.dict[text]) {
+      // debugLog.info('using local dictinary')
+      return this.dict[text];
+    }
 
-    this.appendToDict({ cn: text, en: enKey });
-    return enKey;
+    if (engineIndex >= this.engines.length) {
+      throw new Error('translate error, all translate engine can not access');
+    }
+
+    let enKey;
+    let index = engineIndex;
+
+    try {
+      let res = await this.engines[index].translate(text);
+      enKey = this.startCaseClassName(res.result[0]);
+      this.appendToDict({ cn: text, en: enKey });
+      return enKey;
+    } catch (err) {
+      this.translateAsync(text, index++);
+    }
   }
 }
 
