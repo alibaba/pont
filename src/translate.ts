f187b4bd1a61b1e302cefac61ee0e102f6843f63
gaoletian <gaoletian@finupgroup.com>
2019-02-25 16:23:06 +0800

    fix: replace node-google-translate-china with translation.js && update test case

--- src/translate.ts
+++ src/translate.ts
@@ -1,56 +1,45 @@
 import * as _ from 'lodash';
-import googleTranslate = require('node-google-translate-china');
+const { google } = require('translation.js');
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as os from 'os';
 
 export class Translate {
+  private localDictDir = os.homedir() + '/.pont';
+  private dict: { [key: string]: string } = {};
+  private dictFullPath = '';
 
-    private localDictDir = os.homedir() + '/.pont';
-    private dict: { [key: string]: string } = {}
-    private dictFullPath = ''
+  constructor(dictName = 'dict.json') {
+    fs.mkdirpSync(this.localDictDir);
+    this.dictFullPath = path.normalize(this.localDictDir + '/' + dictName);
+    this.dict = fs.pathExistsSync(this.dictFullPath) ? this.loadDict() : {};
+  }
 
-    constructor(dictName = 'dict.json') {
-        fs.mkdirpSync(this.localDictDir);
-        this.dictFullPath = path.normalize(this.localDictDir + '/' + dictName);
-        this.dict = fs.pathExistsSync(this.dictFullPath)
-            ? this.loadDict()
-            : {}
+  loadDict() {
+    let dictstr = fs.readFileSync(this.dictFullPath, { encoding: 'utf8' });
+    dictstr = dictstr.slice(0, dictstr.length - 2);
+    return JSON.parse(`{${dictstr}}`);
+  }
+
+  appendToDict(pairKey: { cn: string; en: string }) {
+    fs.appendFileSync(this.dictFullPath, `"${pairKey.cn}": "${pairKey.en}",\n`);
+  }
+
+  async translateAsync(text: string) {
+    if (this.dict[text]) return this.dict[text];
+
+    let res = await google.translate(text);
+
+    let wordArray = _.startCase(res.result[0]).split(' ');
+    if (wordArray.length > 6) {
+      wordArray = [].concat(wordArray.slice(0, 5), wordArray.slice(-1));
     }
 
-    loadDict() {
-        let dictstr = fs.readFileSync(this.dictFullPath, { encoding: 'utf8' })
-        dictstr = dictstr.slice(0, dictstr.length - 2)
-        return JSON.parse(`{${dictstr}}`);
-    }
+    let enKey = wordArray.join('');
 
-    appendToDict(pairKey: { cn: string, en: string }) {
-        fs.appendFileSync(this.dictFullPath, `"${pairKey.cn}": "${pairKey.en}",\n`)
-    }
-
-    translateAsync(text: string): Promise<string> {
-
-        if (this.dict[text]) return Promise.resolve(this.dict[text])
-
-        return new Promise((resolve) => {
-            googleTranslate({
-                text,
-                source: 'zh-CN',
-                target: 'en'
-            }, (result: any) => {
-                let wordArray = _.startCase(result.translation).split(' ');
-                if (wordArray.length > 6) {
-                    wordArray = [].concat(wordArray.slice(0, 5), wordArray.slice(-1))
-                }
-
-                let enKey = wordArray.join('');
-
-                this.appendToDict({ cn: text, en: enKey });
-
-                resolve(enKey);
-            });
-        })
-    }
+    this.appendToDict({ cn: text, en: enKey });
+    return enKey;
+  }
 }
 
 export default new Translate();
