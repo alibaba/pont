92dc7b92aa49a85087649a38084c72a8b5c5ba09
jasonHzq <hzongquan@126.com>
2019-05-08 22:03:33 +0800

    fix: fix version

--- pontTemplate.ts
+++
@@ -1,9 +0,0 @@
-
-import * as Pont from 'pont-engine';
-import { CodeGenerator, Interface } from "pont-engine";
-
-export class FileStructures extends Pont.FileStructures {
-}
-
-export default class MyGenerator extends CodeGenerator {
-}
--- transformTemplate.ts
+++
@@ -1,6 +0,0 @@
-
-import { StandardDataSource } from "pont-engine";
-
-export default function(dataSource: StandardDataSource): StandardDataSource {
-  return dataSource;
-}
