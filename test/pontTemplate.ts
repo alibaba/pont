7dc013f9cdbce50445b906e4f15bd1c36177a574
非壹 <herman.lzm@alibaba-inc.com>
2019-09-02 10:25:45 +0800

    fix: 恢复pontTemplate

--- test/pontTemplate.ts
+++ test/pontTemplate.ts
@@ -1,36 +1,4 @@
-import { CodeGenerator, Interface } from "../";
+import { CodeGenerator } from "../";
 
 export default class MyGenerator extends CodeGenerator {
-  getInterfaceContent(inter: Interface) {
-    const paramsCode = inter.getParamsCode().replace(': file', ': FormData');
-    const bodyParamsCode = inter.getBodyParamsCode();
-    const method = inter.method.toUpperCase();
-
-    return `
-      /**
-      * @description ${inter.description}
-      */
-    
-    import { getUrl } from 'src/utils/Url';
-    import * as defs from '../../baseClass';
-
-    export ${paramsCode}
-
-    export const init = ${inter.response.initialValue};
-
-    export function createFetchAction(types, stateKey) {
-      return (params = {}${bodyParamsCode ? `, bodyParams` : ''}, meta?: any) => {
-        return {
-          types,
-          meta,
-          stateKey,
-          method: "${method}",
-          url: getUrl("${inter.path}", params, "${method}"),
-          ${bodyParamsCode ? 'params: bodyParams,' : 'params,'}
-          init,
-        };
-      };
-    }
-   `;
-  }
 }
