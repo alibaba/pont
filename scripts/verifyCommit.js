af90312356fe51c7690671324400f74c1bd1bb4d
gaoletian <gaoletian@finupgroup.com>
2019-12-20 20:35:26 +0800

    wip: mono-repo workflow

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -6,15 +6,13 @@ const msg = require('fs')
   .readFileSync(msgPath, 'utf-8')
   .trim();
 
-const RegStr = `/^(revert: )?(feat|fix|docs|refactor|perf|test|workflow|build|ci|chore|wip|release)(\(.+\))?: .{1,50}/`;
-
-const commitRE = new RegExp(RegStr);
+const commitRE = /^(release: )?(revert: )?(feat|fix|docs|refactor|perf|test|workflow|build|ci|chore|wip|release)(\(.+\))?: .{1,50}/;
 
 if (!commitRE.test(msg)) {
   console.log();
   console.error(
     `  ${chalk.bgRed.white(' ERROR ')} ${chalk.red(`commit 格式不合法 `)}\n\n` +
-      `  ${chalk.bgBlue.white(` ${RegStr} `)}\n\n` +
+      `  ${chalk.bgBlue.white(` ${commitRE} `)}\n\n` +
       chalk.red(`  Proper commit message format is required for automated changelog generation. Examples:\n\n`) +
       `    ${chalk.green(`feat(core): `)}\n` +
       `    ${chalk.green(`fix(plugin): handle some error when sometime (close #28)`)}\n\n` +
