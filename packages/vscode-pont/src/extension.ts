324ebb9e6a97eabaf9c5c4d3eda0c34c79ea0ccb
fuying.yfy <fuying.yfy@alibaba-inc.com>
2022-04-19 16:53:20 +0800

    feat: 添加npm自动更新

--- packages/vscode-pont/src/extension.ts
+++ packages/vscode-pont/src/extension.ts
@@ -2,14 +2,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { Manager, Config, lookForFiles } from 'pont-engine';
-import * as path from 'path';
-import { showProgress, verifyPontEngineVersion } from './utils';
-import { MocksServer } from './mocks';
+import { lookForFiles } from 'pont-engine';
+import { syncNpm, verifyPontEngineVersion } from './utils';
 import { CommandCenter } from './commands';
 import { setContext } from './utils/setContext';
 import { initViews } from './views';
-import { getPontOriginsProvider } from './views/pontOrigins';
 
 const managerCleanUps: vscode.Disposable[] = [];
 
@@ -20,8 +17,8 @@ function doCleanUp() {
 export async function activate(context: vscode.ExtensionContext) {
   console.log('extension "Pont" is now active!');
 
-  setContext('versionError', true);
-  setContext('noConfigFile', true);
+  setContext('versionError', false);
+  setContext('noConfigFile', false);
 
   const disposables: vscode.Disposable[] = [];
   context.subscriptions.push(new vscode.Disposable(() => vscode.Disposable.from(...disposables).dispose()));
@@ -40,12 +37,16 @@ export async function activate(context: vscode.ExtensionContext) {
 
   commandCenter.setConfigPath(configPath);
 
+  await syncNpm();
+
+  const versionError = !verifyPontEngineVersion();
+  const noConfigFile = !configPath;
   // 没有安装 pont-engine 或 版本不一致。安装当前vscode插件对应版本的pont-engine
-  setContext('versionError', verifyPontEngineVersion());
+  setContext('versionError', versionError);
 
-  setContext('noConfigFile', !!configPath);
+  setContext('noConfigFile', noConfigFile);
 
-  if (configPath) {
+  if (!noConfigFile && !versionError) {
     commandCenter.createManager();
   }
 
